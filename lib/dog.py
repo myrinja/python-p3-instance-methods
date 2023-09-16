a29a31c722e16fa79fef60d9e265081bdc2db349
myrinjau <myrinjau98@gmail.com>
2023-09-16 20:54:04 +0300

    add instance method for dog class

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,12 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
+fido = Dog()
+fido.bark()
 
     #Instance method definition
-    pass
+    
